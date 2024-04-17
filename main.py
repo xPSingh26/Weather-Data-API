58dca4c0d9cabea7200bfc2734ba1b4a31eafd9b
Praval <p.singhx26@gmail.com>
2024-04-17 19:35:11 +0530

    add url endpoints for yearly data and single station data

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 stations = pd.read_csv("Weather Data/Data/stations.txt", skiprows=17)
 stations = stations[["STAID","STANAME                                 "]][0:100]
 
+
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
@@ -24,6 +25,29 @@ def data(station, date):
     return {"station": station,
             "date": date,
             "temperature": temp}
+
+
+@app.route("/api/v1/<station>")
+def stat_data(station):
+    filename = "TG_STAID" + station.zfill(6)
+
+    df = pd.read_csv(rf"Weather Data/Data/{filename}.txt", skiprows=20,
+                     parse_dates=['    DATE'])
+
+    df["TG0"] = df['   TG'].mask(df['   TG'] == -9999, np.nan) / 10
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def year_data(station, year):
+    filename = "TG_STAID" + station.zfill(6)
+    df = pd.read_csv(rf"Weather Data/Data/{filename}.txt", skiprows=20,)
+    df["    DATE"] = df["    DATE"].astype(str)
+    df["TG0"] = df['   TG'].mask(df['   TG'] == -9999, np.nan) / 10
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+
+    return result
 
 
 if __name__ == "__main__":
